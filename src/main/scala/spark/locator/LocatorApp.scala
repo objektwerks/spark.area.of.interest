eb147244029992254a418b085caf33f573f18426
objektwerks <objektwerks@runbox.com>
2018-07-24 08:08:36 -0400

    refactored locator app to read in areas of interest

--- src/main/scala/spark/locator/LocatorApp.scala
+++ src/main/scala/spark/locator/LocatorApp.scala
@@ -5,4 +5,12 @@ import org.apache.spark.sql.SparkSession
 object LocatorApp extends App {
   val sparkSession = SparkSession.builder.master("local[*]").appName("Locator").getOrCreate()
   sys.addShutdownHook(sparkSession.stop)
+
+  import AreaOfInterest._
+  val areasOfInterest = sparkSession
+    .read
+    .option("header", true)
+    .schema(areaOfInterestStructType)
+    .csv("./data/areas_of_interest.txt")
+    .as[AreaOfInterest]
 }
