feb0de1e0019478c6146b9eb2564f87b603cf7b1
objektwerks <objektwerks@runbox.com>
2018-07-24 06:25:46 -0400

    added struct type to location and area of interest

--- src/main/scala/spark/locator/Location.scala
+++ src/main/scala/spark/locator/Location.scala
@@ -2,6 +2,16 @@ package spark.locator
 
 import java.time.Instant
 
+import org.apache.spark.sql.Encoders
+
 case class Location(advertiserId: String, locationAt: Instant, latitude: Double, longitude: Double)
 
+object Location {
+  val locationStructType = Encoders.product[Location].schema
+}
+
 case class AreaOfInterest(name: String, latitude: Double, longitude: Double, radius: Double)
+
+object AreaOfInterest {
+  val areaOfInterestStructType = Encoders.product[AreaOfInterest].schema
+}
