cf17c10c9e69437f8d55c349d779c0b6501f2285
objektwerks <objektwerks@runbox.com>
2019-08-03 17:26:48 -0400

    edited run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ spark-submit \
   --class aoi.AreaOfInterestApp \
   --master local[2] \
   --packages com.typesafe:config:1.3.4 \
-  ./spark.area.of.interest/target/scala-2.12/spark-area-of-interest_2.12-0.1.jar
+  ./target/scala-2.12/spark-area-of-interest_2.12-0.1.jar
