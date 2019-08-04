6ac6e105db67e8cee2950c1aae41a282f7df7dbc
objektwerks <objektwerks@runbox.com>
2019-08-04 10:00:53 -0400

    replaced scala 2.12 with 2.11; edited submit.sh; successfully executed ./submit.sh

--- submit.sh
+++ submit.sh
@@ -3,4 +3,4 @@ spark-submit \
   --class aoi.AreaOfInterestApp \
   --master local[2] \
   --packages com.typesafe:config:1.3.4 \
-  ./target/scala-2.12/spark-area-of-interest_2.12-0.1.jar
+  ./target/scala-2.11/spark-area-of-interest_2.11-0.1.jar
