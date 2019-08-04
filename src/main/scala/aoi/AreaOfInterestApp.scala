a63cf642868f2c6653602ada7381478262c64adb
objektwerks <objektwerks@runbox.com>
2019-08-04 09:24:39 -0400

    replaced App with a main method

--- src/main/scala/aoi/AreaOfInterestApp.scala
+++ src/main/scala/aoi/AreaOfInterestApp.scala
@@ -5,55 +5,59 @@ import org.apache.spark.sql.SparkSession
 
 import scala.util.Try
 
-object AreaOfInterestApp extends App {
-  val conf = ConfigFactory.load("app.conf").getConfig("app")
-  val areaOfInterestRadiusInKilometers = Try(args(0).toDouble).getOrElse(25.0)
-  val hitDaysHence = Try(daysToEpochMillis(args(1).toLong)).getOrElse(daysToEpochMillis(365))
+object AreaOfInterestApp {
+  def main(args: Array[String]): Unit = {
+    val areaOfInterestRadiusInKilometers = Try(args(0).toDouble).getOrElse(25.0)
+    val hitDaysHence = Try(daysToEpochMillis(args(1).toLong)).getOrElse(daysToEpochMillis(365))
 
-  val sparkEventLogDir = conf.getString("spark.eventLog.dir")
-  val sparkEventDirCreated = createSparkEventsDir(sparkEventLogDir)
-  println(s"*** $sparkEventLogDir exists or was created: $sparkEventDirCreated")
+    val conf = ConfigFactory.load("app.conf").getConfig("app")
 
-  val sparkSession = SparkSession
-    .builder
-    .master(conf.getString("master"))
-    .appName(conf.getString("name"))
-    .config("spark.eventLog.enabled", conf.getBoolean("spark.eventLog.enabled"))
-    .config("spark.eventLog.dir", sparkEventLogDir)
-    .getOrCreate()
-  println("Initialized Spark AreaOfInterestApp. Press Ctrl C to terminate.")
+    val sparkEventLogDir = conf.getString("spark.eventLog.dir")
+    val sparkEventDirCreated = createSparkEventsDir(sparkEventLogDir)
+    println(s"*** $sparkEventLogDir exists or was created: $sparkEventDirCreated")
 
-  sys.addShutdownHook {
-    sparkSession.stop
-    println("Terminated Spark AreaOfInterestApp.")
+    val sparkSession = SparkSession
+      .builder
+      .master(conf.getString("master"))
+      .appName(conf.getString("name"))
+      .config("spark.eventLog.enabled", conf.getBoolean("spark.eventLog.enabled"))
+      .config("spark.eventLog.dir", sparkEventLogDir)
+      .getOrCreate()
+    println("Initialized Spark AreaOfInterestApp. Press Ctrl C to terminate.")
+
+    sys.addShutdownHook {
+      sparkSession.stop
+      println("Terminated Spark AreaOfInterestApp.")
+    }
+
+    import sparkSession.implicits._
+
+    val areasOfInterest = sparkSession
+      .read
+      .option("header", true)
+      .schema(areaOfInterestStructType)
+      .csv(conf.getString("csv"))
+      .as[AreaOfInterest]
+      .collect
+      .toList
+
+    val areaOfInterestsToHit = mapAreaOfInterestsToHit(areasOfInterest, areaOfInterestRadiusInKilometers)(_:Hit)
+
+    val hits = sparkSession
+      .readStream
+      .option("basePath", conf.getString("hits"))
+      .option("header", true)
+      .schema(hitStructType)
+      .csv(conf.getString("hits"))
+      .as[Hit]
+      .filter(hit => hit.utc > hitDaysHence)
+      .map(hit => areaOfInterestsToHit(hit))
+      .as[Map[AreaOfInterest, Hit]]
+      .writeStream
+      .foreach(areaOfInterestsToHitForeachWriter)
+      .start
+    hits.awaitTermination
+
+    ()
   }
-
-  import sparkSession.implicits._
-
-  val areasOfInterest = sparkSession
-    .read
-    .option("header", true)
-    .schema(areaOfInterestStructType)
-    .csv(conf.getString("csv"))
-    .as[AreaOfInterest]
-    .collect
-    .toList
-
-  val areaOfInterestsToHit = mapAreaOfInterestsToHit(areasOfInterest, areaOfInterestRadiusInKilometers)(_:Hit)
-
-  val hits = sparkSession
-    .readStream
-    .option("basePath", conf.getString("hits"))
-    .option("header", true)
-    .schema(hitStructType)
-    .csv(conf.getString("hits"))
-    .as[Hit]
-    .filter(hit => hit.utc > hitDaysHence)
-    .map(hit => areaOfInterestsToHit(hit))
-    .as[Map[AreaOfInterest, Hit]]
-    .writeStream
-    .foreach(areaOfInterestsToHitForeachWriter)
-    .start
-
-  hits.awaitTermination
 }
